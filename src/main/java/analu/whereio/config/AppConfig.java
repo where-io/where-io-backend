b994de472ca2859d82d77cbdde426071575b6298
LccsBueno <luccas.lima@sptech.school>
2026-05-16 16:01:37 -0300

    config: add Clock bean for testable time injection

---
+++ src/main/java/analu/whereio/config/AppConfig.java
@@ -0,0 +1,15 @@
+package analu.whereio.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Clock;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Clock clock() {
+        return Clock.systemUTC();
+    }
+}
