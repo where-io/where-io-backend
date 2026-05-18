c773a68d7d2e5bb25442700bde9680324bc6369b
LccsBueno <luccas.lima@sptech.school>
2026-05-18 15:08:19 -0300

    adicionanod 8081 no CORS

--- src/main/java/analu/whereio/config/CorsConfig.java
+++ src/main/java/analu/whereio/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://localhost:8081")
+                        .allowedOrigins("http://localhost:3000", "http://localhost:8081", "http://localhost:8082")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
